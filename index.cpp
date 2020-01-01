88ffb327bcce83e180b94b4ae6dc730b3792961d
Ndekere254 <ndekerekiritu@gmail.com>
2020-01-01 14:04:14 +0300

    Characters and Strings in CPP

---
+++ index.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+//main() is a Fuction
+
+int main() {
+	/*string playerName = "zxvdv";
+	
+	playerName = "Gakure,,,";
+	cout << playerName << endl;*/
+	string jinaLako = "Dirangu";
+	cout << "Your name is " << jinaLako.length() << " Letters Long! \n";
+}
